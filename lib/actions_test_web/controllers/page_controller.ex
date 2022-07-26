1a5bac47f5d27baa236514060cc8d5dfe20ef952
Steven Fuchs <stwf@yahoo.com>
2022-07-26 08:20:58 -0400

    big changes

--- lib/actions_test_web/controllers/page_controller.ex
+++ lib/actions_test_web/controllers/page_controller.ex
@@ -4,4 +4,8 @@ defmodule ActionsTestWeb.PageController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
+
+  def show(conn, _params) do
+    render(conn, "index.html")
+  end
 end
